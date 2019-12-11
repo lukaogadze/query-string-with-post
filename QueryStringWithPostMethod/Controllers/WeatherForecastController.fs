dcf632dfa5888c3880e676d1a970d13e0dc16cec
loghadze <loghadze@justice.gov.ge>
2019-12-12 02:16:29 +0400

    initial

---
+++ QueryStringWithPostMethod/Controllers/WeatherForecastController.fs
@@ -0,0 +1,35 @@
+﻿namespace QueryStringWithPostMethod.Controllers
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Threading.Tasks
+open QueryStringWithPostMethod.Models
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open QueryStringWithPostMethod
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+
+    let summaries = [| "Freezing"; "Bracing"; "Chilly"; "Cool"; "Mild"; "Warm"; "Balmy"; "Hot"; "Sweltering"; "Scorching" |]
+
+    [<HttpGet>]
+    member __.Get() : WeatherForecast[] =
+        let rng = System.Random()       
+        [|
+            for index in 0..4 ->
+                { Date = DateTime.Now.AddDays(float index)
+                  TemperatureC = rng.Next(-20,55)
+                  Summary = summaries.[rng.Next(summaries.Length)] }
+        |]
+
+    // http://localhost:5000/weatherforecast/create?nickName=mdaaa
+    [<HttpPost("create")>]
+    member __.Create(nickName: string, person: Person): IActionResult =
+        File.WriteAllText("nickName", nickName)
+        File.WriteAllText("FirstName", person.FirstName)
+        File.WriteAllText("LastName", person.LastName)
+        __.Ok() :> IActionResult
---
+++ QueryStringWithPostMethod/Models/Person.fs
@@ -0,0 +1,5 @@
+﻿namespace QueryStringWithPostMethod.Models
+
+type Person() = 
+    member val FirstName: string = null with get, set
+    member val LastName: string = null with get, set
